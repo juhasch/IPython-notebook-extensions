de73c96a99f6527b7c594b30c94b2e83bd5b8f90
Juergen Hasch <python@elbonia.de>
2015-01-24 12:18:54 +0100

    Change URL name for previews

--- preview/preview.py
+++ preview/preview.py
@@ -91,5 +91,5 @@ def load_jupyter_server_extension(nbapp):
     webapp = nbapp.web_app
     base_url = webapp.settings['base_url']
     webapp.add_handlers(".*$", [
-        (ujoin(base_url, r"/listing/"), ListingHandler)
+        (ujoin(base_url, r"/preview/"), ListingHandler)
     ])
